29c9c1860e83980bc98171f951b45b2526169117
Kayque Dias <kayquedias204@gmail.com>
2021-12-12 13:31:45 -0300

    night mode

--- night-mode.js
+++ night-mode.js
@@ -10,15 +10,15 @@ function modoNoturno() {
     if (checkbox.checked) {
         divOndaVerde.style = `background: url('../imagens/Green-Wave-Dark.png') no-repeat;`;
         divOndaAmarela.style = `background: url('../imagens/Yellow-Wave-Dark.png') no-repeat; background-size: 100% 200px;`;
-        iconeBuscar.src = '../imagens/search-black.png';
-        menu.src = '../imagens/menu-hamburguer.png';
+        iconeBuscar.src = '../imagens/search-black.PNG';
+        menu.src = '../imagens/menu-hamburguer.PNG';
         mainBg.style = 'background-color: #000000'
         main.style = 'background-color: #000000'
     } else {
         divOndaVerde.style = 'background: url(../imagens/onda-verde-cortada.png) no-repeat;';
         divOndaAmarela.style = 'background: url(../imagens/onda-laranja-cortada.png) no-repeat; background-size: 100% 100%';
-        iconeBuscar.src = '../imagens/search-white.png';
-        menu.src = '../imagens/menu-white.png';
+        iconeBuscar.src = '../imagens/search-white.PNG';
+        menu.src = '../imagens/menu-white.PNG';
         mainBg.style = 'background-color: #ffffff'
         main.style = 'background-color: #ffffff'
 
