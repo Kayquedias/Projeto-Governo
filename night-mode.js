b1de7d90e717e53846910591c99df882e6bee168
Kayque Dias <kayquedias204@gmail.com>
2021-12-13 12:53:42 -0300

    responsividade

--- night-mode.js
+++ night-mode.js
@@ -4,22 +4,22 @@ function modoNoturno() {
     const checkbox = document.querySelector('#nightmode');
     const iconeBuscar = document.querySelector('img.buscador');
     const menu = document.querySelector('#menu');
-    const mainBg = document.querySelector('body')
+    const body = document.querySelector('body')
     const main = document.querySelector('main')
     
     if (checkbox.checked) {
-        divOndaVerde.style = `background: url('../imagens/Green-Wave-Dark.png') no-repeat;`;
-        divOndaAmarela.style = `background: url('./imagens/Yellow-Wave-Dark.png') no-repeat; background-size: 100% 200px;`;
-        iconeBuscar.src = './imagens/search-black.png';
-        menu.src = './imagens/menu-hamburguer.png';
-        mainBg.style = 'background-color: #000000'
+        divOndaVerde.style = `background: url('../imagens/Green-Wave-Dark.png') no-repeat;`
+        divOndaAmarela.style = `background: url('../imagens/Yellow-Wave-Dark.png') no-repeat; background-size: 100% 100%;`
+        iconeBuscar.src = '../imagens/search-black.png';
+        menu.src = '../imagens/menu-hamburguer.png';
+        body.style = 'background-color: #000000'
         main.style = 'background-color: #000000'
     } else {
-        divOndaVerde.style = 'background: url(./imagens/onda-verde-cortada.png) no-repeat;';
-        divOndaAmarela.style = 'background: url(./imagens/onda-laranja-cortada.png) no-repeat; background-size: 100% 100%';
-        iconeBuscar.src = './imagens/search-white.png';
-        menu.src = './imagens/menu-white.png';
-        mainBg.style = 'background-color: #ffffff'
+        divOndaVerde.style = `background: url('../imagens/onda-verde-cortada.png') no-repeat; background-size 100% 11rem;`
+        divOndaAmarela.style = `background: url('../imagens/onda-laranja-cortada.png') no-repeat; background-size: 100% 100%`
+        iconeBuscar.src = '../imagens/search-white.png';
+        menu.src = '../imagens/menu-white.png';
+        body.style = 'background-color: #ffffff'
         main.style = 'background-color: #ffffff'
 
     };
