507b8f4de49eca01d3a8bc27faa8019a5fd6e268
Kayque Dias <kayquedias204@gmail.com>
2021-12-13 22:04:05 -0300

    ajustes

--- night-mode.js
+++ night-mode.js
@@ -2,10 +2,10 @@ function modoNoturno() {
     const divOndaVerde = document.querySelector('.onda1');
     const divOndaAmarela = document.querySelector('.onda2');
 
-    const publi1 = document.querySelector('.img-publi1');
-    const publi2 = document.querySelector('.img-publi2');
-    const publi3 = document.querySelector('.img-publi3');
-    const publi4 = document.querySelector('.img-publi4');
+    const img1 = document.querySelector('.img-publi1');
+    const img2 = document.querySelector('.img-publi2');
+    const img3 = document.querySelector('.img-publi3');
+    const img4 = document.querySelector('.img-publi4');
 
     const checkbox = document.querySelector('#nightmode');
 
@@ -20,10 +20,10 @@ function modoNoturno() {
         iconeBuscar.src = '../imagens/search-black.png';
         menu.src = '../imagens/menu-hamburguer.png';
 
-        publi1.src = '../imagens/icon-1.5.png';
-        publi2.src = '../imagens/icon-2.5.png';
-        publi3.src = '../imagens/icon-3.1.png';
-        publi4.src = '../imagens/icon-4.1.png';
+        img1.src = '../imagens/icon-1.5.png';
+        img2.src = '../imagens/icon-2.5.png';
+        img3.src = '../imagens/icon-3,1.png';
+        img4.src = '../imagens/icon-4.1.png';
 
         body.style = 'background-color: #000000'
         main.style = 'background-color: #000000'
@@ -33,10 +33,10 @@ function modoNoturno() {
         iconeBuscar.src = '../imagens/search-white.png';
         menu.src = '../imagens/menu-white.png';
 
-        publi1.src = '../imagens/icon-1.png';
-        publi2.src = '../imagens/icon-2.png';
-        publi3.src = '../imagens/icon-3.png';
-        publi4.src = '../imagens/icon-4.png';
+        img1.src = '../imagens/icon-1.png';
+        img2.src = '../imagens/icon-2.png';
+        img3.src = '../imagens/icon-3.png';
+        img4.src = '../imagens/icon-4.png';
 
         body.style = 'background-color: #ffffff'
         main.style = 'background-color: #ffffff'
